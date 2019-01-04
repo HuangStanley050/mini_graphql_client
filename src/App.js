09241a11c647d1cf73d2408618870ecdb9a95563
Stanley Huang <infamous_godhand@yahoo.com>
2019-01-04 00:03:09 +0000

    start with basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import { Route, Redirect, Switch } from "react-router-dom";
+import AuthPage from "./components/auth";
+import BookingPage from "./components/bookings";
+import EventPage from "./components/events";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div>
+        <Switch>
+          <Redirect exact from="/" to="/auth" />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventPage} />
+          <Route path="/bookings" component={BookingPage} />
+        </Switch>
+      </div>
+    );
   }
 }
 
