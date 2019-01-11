c09ae7a8b7e8d9613887386fdcbb803939ad5948
Stanley Huang <infamous_godhand@yahoo.com>
2019-01-11 07:53:36 +0000

    auth component done

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
-
+import axios from "axios";
+import "./auth.css";
 class AuthPage extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
   render() {
-    return <h1>Auth Page</h1>;
+    return (
+      <form className="auth-form" onSubmit={this.handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={this.handleInput}
+            value={this.state.email}
+            type="email"
+            id="email"
+            name="email"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={this.handleInput}
+            value={this.state.password}
+            type="password"
+            id="password"
+            name="password"
+          />
+        </div>
+        <div className="form-action">
+          <button type="submit">Submit</button>
+          <button type="button">Switch to SignUp</button>
+        </div>
+      </form>
+    );
   }
 }
 
