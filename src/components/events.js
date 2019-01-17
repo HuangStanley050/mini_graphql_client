eb266ac205a8180304fdc7b7c8e75789a529ac71
Stanley Huang <infamous_godhand@yahoo.com>
2019-01-17 23:16:54 +0000

    modal roughly done

--- src/components/events.js
+++ src/components/events.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import Modal from "./modal/modal";
+import "./events.css";
 
 class EventPage extends Component {
   render() {
-    return <h1>Event Page</h1>;
+    return (
+      <React.Fragment>
+        <Modal>
+          <p>Modal Content</p>
+        </Modal>
+        <div className="events-control">
+          <p>Share your event</p>
+          <button className="btn">Create Event</button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
